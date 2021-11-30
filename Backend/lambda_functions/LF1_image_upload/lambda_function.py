95156d08d8199f8e96bede506fa6a72b26fe8b9a
Adam Carpentieri <ajc301@gmail.com>
2021-11-29 23:04:50 -0500

    initial rds implementation - still needs some work

--- Backend/lambda_functions/LF1_image_upload/lambda_function.py
+++ Backend/lambda_functions/LF1_image_upload/lambda_function.py
@@ -1,8 +1,16 @@
 import json
 import urllib.parse
 import boto3
+import pymysql
 
 s3=boto3.client('s3')
+
+rdsConn = pymysql.connect(host='trading-cards.c49euq66g8jj.us-east-1.rds.amazonaws.com',
+                             user='admin',
+                             password='c3iTGk4gKXp4JRH',
+                             database='mint_condition',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 def lambda_handler(event, context):
 
@@ -10,6 +18,9 @@ def lambda_handler(event, context):
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
 
     condition = invoke_sagemaker(bucket, key)
+
+    #TODO make sure condition is a float value
+
     upload_to_rds(bucket, key, condition)
     upload_to_opensearch(bucket, key, condition)
 
@@ -25,10 +36,29 @@ def invoke_sagemaker(bucket, key):
 
 
 def upload_to_rds(bucket, key, condition):
+
     s3_data = get_s3_metadata(key, bucket)
     label = s3_data['ResponseMetadata']['HTTPHeaders']['x-amz-meta-customlabels'] if 'x-amz-meta-customlabels' in s3_data['ResponseMetadata']['HTTPHeaders'] else ""
     time_created = s3_data['LastModified']
     user_id = s3_data['ResponseMetadata']['HTTPHeaders']['x-amz-meta-user']
+
+    condition_id = None
+
+    with rdsConn:
+
+        #TODO implement failure handling - what if nothing is retrieved
+        with rdsConn.cursor() as cursor:
+            sql = "SELECT `condition_id` FROM `card_conditions` WHERE `card_condition_rating` = %s"
+            cursor.execute(sql, (str(condition),))
+            result = cursor.fetchone()
+            condition_id = result.condition_id
+
+        with rdsConn.cursor() as cursor:
+            sql = "INSERT INTO `cards` (`card_label`, `card_img_path`, `card_condition_id`, `user_id`, `time_created`) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (label, "s3://" + bucket + "/" + key, str(condition_id), str(user_id), str(time_created)))
+
+        rdsConn.commit()
+
     print(label)
     print(user_id)
     print(time_created)
--- Backend/lambda_functions/LF3_query_cards/lambda_function.py
+++ Backend/lambda_functions/LF3_query_cards/lambda_function.py
@@ -1,36 +1,145 @@
 import json
-
+import pymysql
 
 '''
 Refer to https://stackoverflow.com/questions/49715482/how-to-access-the-url-that-invoked-my-lambda-function for event schema
 '''
 
+rdsConn = pymysql.connect(host='trading-cards.c49euq66g8jj.us-east-1.rds.amazonaws.com',
+                             user='admin',
+                             password='c3iTGk4gKXp4JRH',
+                             database='mint_condition',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
 def lambda_handler(event, context):
     print(event)
     path =  event["path"]
-    user = event['headers']['x-amz-meta-user']
+    user_id = event['headers']['x-amz-meta-user']
     httpMethod = event['httpMethod']
+
+    #TODO get user_id from incognito, if not avail or does not exist throw error
+
+    #TODO: price range, pagination, date range
     if path == "/cards":
         if httpMethod == "GET":
-            #TODO implement getting all cards for user
-            return dummy_response()
+            
+             with rdsConn:
+                with rdsConn.cursor() as cursor:
+                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                    `users`.`name` as `owner_name`, `cards`.`card_id`, `card_img_path` as `url`, `card_value` as `value`
+                    FROM `cards`
+                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                    LEFT JOIN `users` ON `cards.`user_id` = `users`.`user_id`
+                    WHERE `cards`.`user_id` = %s"""
+                    cursor.execute(sql, (str(user_id),))
+                    cards = { "cards" : [] } if cursor.rowcount == 0 else { "cards": cursor.fetchall() }
+                    return real_response(cards)
         else:
             return raise_method_not_allowed()
-
+    
     elif path == "/card":
-        if httpMethod == "POST":
-            #TODO update card for specific user
-            return dummy_response()
+        if httpMethod == "POST": #assuming comma separated vals
+
+            #TODO get card for specified id (/card/{id}). get labels too
+            card_id = 1
+            labels = ['a', 'b']
+            
+            additionalLabels = ",".join(labels)
+
+            with rdsConn:
+
+                with rdsConn.cursor() as cursor:
+                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                    `users`.`name` as `owner_name`, `cards`.`card_id`, `card_img_path` as `url`, `card_value` as `value`
+                    FROM `cards`
+                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                    LEFT JOIN `users` ON `cards.`user_id` = `users`.`user_id`
+                    WHERE `cards`.`user_id` = %s
+                        AND `cards`.`card_id` = %s"""
+                    cursor.execute(sql, (str(user_id), str(card_id),))
+                    if cursor.rowcount == 0:
+                        return unexpected_error("card not found for user")
+                    
+                    card = cursor.fetchone()
+
+                    sql = "UPDATE `cards` SET `card_label` = CONCAT_WS(',', `card_label`, %s) WHERE `card_id` = %s"
+                    cursor.execute(sql, (additionalLabels, str(card_id),))
+
+                rdsConn.commit()
+
+                #TODO update opensearch too
+
+                with rdsConn.cursor() as cursor:
+                    
+                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                    `users`.`name` as `owner_name`, `cards`.`card_id`, `card_img_path` as `url`, `card_value` as `value`
+                    FROM `cards`
+                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                    LEFT JOIN `users` ON `cards.`user_id` = `users`.`user_id`
+                    WHERE `cards`.`user_id` = %s
+                        AND `cards`.`card_id` = %s"""
+                    cursor.execute(sql, (str(user_id), str(card_id),))
+
+                    #bad implementation: theoretically could have been deleted between this and first part (cut corners)
+                    return real_response(cursor.fetchone())
+                    
+
+                return real_response(card)
+
         else:
             return raise_method_not_allowed()
 
     elif path.contains("/card/"):
         if httpMethod == "GET":
-            #TODO get card for specified id (/card/{id}). Check if user has access to specified ID
-            return dummy_response()
+
+            #TODO get card for specified id (/card/{id}). 
+            card_id = 1
+
+            with rdsConn:
+                with rdsConn.cursor() as cursor:
+                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                    `users`.`name` as `owner_name`, `cards`.`card_id`, `card_img_path` as `url`, `card_value` as `value`
+                    FROM `cards`
+                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                    LEFT JOIN `users` ON `cards.`user_id` = `users`.`user_id`
+                    WHERE `cards`.`user_id` = %s
+                        AND `cards`.`card_id` = %s"""
+                    cursor.execute(sql, (str(user_id), str(card_id),))
+                    card = {} if cursor.rowcount == 0 else cursor.fetchone()
+                    return real_response(card)
+
         elif httpMethod == "DELETE":
-            #TODO delete card for user based on ID. Check if user has access to specified ID
-            return dummy_response()
+
+            #TODO get card for specified id (/card/{id}).
+            card_id = 1
+            card = {}
+
+            with rdsConn:
+
+                with rdsConn.cursor() as cursor:
+                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                    `users`.`name` as `owner_name`, `cards`.`card_id`, `card_img_path` as `url`, `card_value` as `value`
+                    FROM `cards`
+                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                    LEFT JOIN `users` ON `cards.`user_id` = `users`.`user_id`
+                    WHERE `cards`.`user_id` = %s
+                        AND `cards`.`card_id` = %s"""
+                    cursor.execute(sql, (str(user_id), str(card_id),))
+                    if cursor.rowcount == 0:
+                        return unexpected_error("card not found for user")
+                    
+                    card = cursor.fetchone()
+
+                with rdsConn.cursor() as cursor:
+                    sql = "DELETE FROM `cards` WHERE `card_id` = %s"
+                    cursor.execute(sql, (str(card_id),))
+
+                rdsConn.commit()
+
+                #TODO delete from opensearch too
+
+                return real_response(card)
         else:
             return raise_method_not_allowed()
 
@@ -44,6 +153,12 @@ def lambda_handler(event, context):
     else:
         return raise_method_not_allowed
 
+
+def unexpected_error(error):
+    return {
+        'statusCode': 500,
+        'body': error
+    }    
 
 def raise_method_not_allowed():
     return {
@@ -61,4 +176,16 @@ def dummy_response():
             'Access-Control-Allow-Methods': 'OPTIONS,PUT,GET'
         },
         'body': "Hi there"
+    }
+
+#assume dict result format
+def real_response(result):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,x-amz-meta-customLabels',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,PUT,GET'
+        },
+        'body': json.dumps(result)
     }
