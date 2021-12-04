6d784e1b117450a959024a8c3f8da8da5d5fe719
Bharathi <bharathimurugan@gmail.com>
2021-12-04 09:58:23 -0500

    updated opensearch search endpoint

--- Backend/lambda_functions/LF3_query_cards/lambda_function.py
+++ Backend/lambda_functions/LF3_query_cards/lambda_function.py
@@ -14,7 +14,7 @@ region = 'us-east-1'
 
 os_host = os.environ['os_url']
 os_username = os.environ['os_username']
-os_pw = os.environ["os.pw"]
+os_pw = os.environ["os_pw"]
 os_index = 'cards/_doc'
 os_url = os_host + os_index
 
@@ -35,17 +35,15 @@ def lambda_handler(event, context):
 
     if path == "/cards":
         if httpMethod == "GET":
-            
-             with rdsConn:
-                with rdsConn.cursor() as cursor:
-                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
-                    user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
-                    FROM `cards`
-                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
-                    WHERE `cards`.`user_id` = %s"""
-                    cursor.execute(sql, (str(user_id),))
-                    cards = { "cards" : [] } if cursor.rowcount == 0 else { "cards": process_cards_obj(cursor.fetchall()) }
-                    return real_response(cards)
+            with rdsConn.cursor() as cursor:
+                sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
+                FROM `cards`
+                LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                WHERE `cards`.`user_id` = %s"""
+                cursor.execute(sql, (str(user_id),))
+                cards = { "cards" : [] } if cursor.rowcount == 0 else { "cards": process_cards_obj(cursor.fetchall()) }
+                return real_response(cards)
         else:
             return raise_method_not_allowed()
     
@@ -61,37 +59,35 @@ def lambda_handler(event, context):
             card_id = event['pathParameters']['id']
             labels = ",".join(event['body']['labels'])
 
-            with rdsConn:
+            with rdsConn.cursor() as cursor:
+                sql = """SELECT `card_id` 
+                FROM `cards`
+                WHERE `cards`.`user_id` = %s
+                    AND `cards`.`card_id` = %s"""
+                cursor.execute(sql, (str(user_id), str(card_id),))
+                if cursor.rowcount == 0:
+                    return unexpected_error("card not found for user")
+                
+                card = cursor.fetchone()
 
-                with rdsConn.cursor() as cursor:
-                    sql = """SELECT `card_id` 
-                    FROM `cards`
-                    WHERE `cards`.`user_id` = %s
-                        AND `cards`.`card_id` = %s"""
-                    cursor.execute(sql, (str(user_id), str(card_id),))
-                    if cursor.rowcount == 0:
-                        return unexpected_error("card not found for user")
-                    
-                    card = cursor.fetchone()
+                sql = "UPDATE `cards` SET `card_label` = %s WHERE `card_id` = %s"
+                cursor.execute(sql, (labels, str(card_id),))
 
-                    sql = "UPDATE `cards` SET `card_label` = %s WHERE `card_id` = %s"
-                    cursor.execute(sql, (labels, str(card_id),))
+            #TODO update opensearch too
 
-                #TODO update opensearch too
+            with rdsConn.cursor() as cursor:
+                
+                sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
+                FROM `cards`
+                LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                WHERE `cards`.`user_id` = %s
+                    AND `cards`.`card_id` = %s"""
+                
+                cursor.execute(sql, (str(user_id), str(card_id),))
 
-                with rdsConn.cursor() as cursor:
-                    
-                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
-                    user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
-                    FROM `cards`
-                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
-                    WHERE `cards`.`user_id` = %s
-                        AND `cards`.`card_id` = %s"""
-                    
-                    cursor.execute(sql, (str(user_id), str(card_id),))
-
-                    #bad implementation: theoretically could have been deleted between this and first part (cut corners)
-                    return real_response(process_card_obj(cursor.fetchone()))
+                #bad implementation: theoretically could have been deleted between this and first part (cut corners)
+                return real_response(process_card_obj(cursor.fetchone()))
 
         else:
             return raise_method_not_allowed()
@@ -104,17 +100,16 @@ def lambda_handler(event, context):
 
             card_id = event['pathParameters']['id']
 
-            with rdsConn:
-                with rdsConn.cursor() as cursor:
-                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
-                    user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
-                    FROM `cards`
-                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
-                    WHERE `cards`.`user_id` = %s
-                        AND `cards`.`card_id` = %s"""
-                    cursor.execute(sql, (str(user_id), str(card_id),))
-                    card = {} if cursor.rowcount == 0 else cursor.fetchone()
-                    return real_response(process_card_obj(card))
+            with rdsConn.cursor() as cursor:
+                sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
+                FROM `cards`
+                LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                WHERE `cards`.`user_id` = %s
+                    AND `cards`.`card_id` = %s"""
+                cursor.execute(sql, (str(user_id), str(card_id),))
+                card = {} if cursor.rowcount == 0 else cursor.fetchone()
+                return real_response(process_card_obj(card))
 
         elif httpMethod == "DELETE":
 
@@ -125,28 +120,26 @@ def lambda_handler(event, context):
 
             card = {}
 
-            with rdsConn:
+            with rdsConn.cursor() as cursor:
+                sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
+                FROM `cards`
+                LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                WHERE `cards`.`user_id` = %s
+                    AND `cards`.`card_id` = %s"""
+                cursor.execute(sql, (str(user_id), str(card_id),))
+                if cursor.rowcount == 0:
+                    return unexpected_error("card not found for user")
+                
+                card = process_card_obj(cursor.fetchone())
 
+            try:
                 with rdsConn.cursor() as cursor:
-                    sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
-                    user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
-                    FROM `cards`
-                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
-                    WHERE `cards`.`user_id` = %s
-                        AND `cards`.`card_id` = %s"""
-                    cursor.execute(sql, (str(user_id), str(card_id),))
-                    if cursor.rowcount == 0:
-                        return unexpected_error("card not found for user")
-                    
-                    card = process_card_obj(cursor.fetchone())
-
-                try:
-                    with rdsConn.cursor() as cursor:
-                        sql = "DELETE FROM `cards` WHERE `card_id` = %s"
-                        cursor.execute(sql, (str(card_id),))
-                    
-                except:
-                    return unexpected_error("deletion failed")
+                    sql = "DELETE FROM `cards` WHERE `card_id` = %s"
+                    cursor.execute(sql, (str(card_id),))
+                
+            except:
+                return unexpected_error("deletion failed")
 
 
             #TODO delete from opensearch too
@@ -164,25 +157,64 @@ def lambda_handler(event, context):
             os_payload = {
                 "size": 10000,
                 "query": {
-                    "match": {
+                    "bool": {
+                        "must": [
+                            {
+                                "match": {
+                                    "user_id": user_id
+                                }
+                            }
+                        ]
                     }
                 }
             }
             if "label" in event["queryStringParameters"]:
-                os_payload["query"]["match"]["labels"] = event["queryStringParameters"]["label"]
+                os_payload["query"]["bool"]["must"].append(
+                    {
+                        "match": {
+                            "labels": event["queryStringParameters"]["label"]
+                        }
+                    }
+                )
             if "condition" in event["queryStringParameters"]:
-                os_payload["query"]["match"]["condition"] = event["queryStringParameters"]["condition"]
-            if not os_payload["query"]["match"]:
-                return real_response(results)
+                os_payload["query"]["bool"]["must"].append(
+                    {
+                        "match": {
+                            "condition": event["queryStringParameters"]["condition"]
+                        }
+                    }
+                )
             results = search_opensearch(os_payload)
+
+            sql = """SELECT `card_condition_name` as `condition_label`, `card_condition_descr` as `condition_desc`, 
+                    user_id as `owner_name`, `cards`.`card_id`, `card_bucket`, `card_s3_key`, `card_value` as `value`
+                    FROM `cards`
+                    LEFT JOIN `card_conditions` ON `cards`.`card_condition_id` = `card_conditions`.`card_condition_id`
+                    WHERE `cards`.`card_id` = %s"""
+
+            execution_params = []
+
+            if results:
+                for i, os_card in enumerate(results):
+                    execution_params.append(os_card['card_id'])
+                    if i != 0:
+                        sql += """OR `cards`.`card_id` = %s"""
+                
+                with rdsConn.cursor() as cursor:
+                    cursor.execute(sql, tuple(execution_params))
+                all_cards = cursor.fetchall()
+                ret_cards = []
+                for card in all_cards:
+                    ret_cards.append(process_card_obj(card))
+
         else:
             return raise_method_not_allowed()
 
-        return real_response(results)
+        return real_response(ret_cards)
 
         # TODO check database for bucket, key, and return a signed url with the image
 
-    elif path.contains("/user/"):
+    elif "/user/" in path:
         user_obj = {
             "user_id": event["requestContext"]["authorizer"]["claims"]["cognito:username"],
             "email": event["requestContext"]["authorizer"]["claims"]["email"],
