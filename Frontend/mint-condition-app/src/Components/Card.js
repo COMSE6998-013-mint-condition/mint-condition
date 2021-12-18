2f7d6df77dad3d077305a4ae5e5fd13bd3bcbcfb
Bharathi <bharathimurugan@gmail.com>
2021-12-18 18:58:34 -0500

    Added chart for history

--- Frontend/mint-condition-app/src/Components/Card.js
+++ Frontend/mint-condition-app/src/Components/Card.js
@@ -1,5 +1,5 @@
 import Grid from '@material-ui/core/Grid';
-import {Box, Container} from '@material-ui/core';
+import {Container} from '@material-ui/core';
 import Header from './Header'
 import CardList from './CardList'
 import Table from '@mui/material/Table';
@@ -8,31 +8,36 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { check_auth_code } from '../utils/auth_helpers';
+import {check_auth_code} from '../utils/auth_helpers';
 import {useLocation, useNavigate} from 'react-router-dom';
-import {Button} from "@mui/material";
+import {Button, Paper} from "@mui/material";
 import axios from "axios";
 import React, {useEffect, useState} from "react";
-import getCards from "./HomePage"
+import {Chart} from "react-google-charts";
 
 function createData(name, max_val, quality, mean_val, quality_desc, min_val, label) {
-  return { name, max_val, quality, mean_val, quality_desc, min_val, label };
+  return {name, max_val, quality, mean_val, quality_desc, min_val, label};
 }
 
-function Card(props){
+function Card(props) {
   check_auth_code();
 
   const navigate = useNavigate()
   const location = useLocation();
   const [cards, setCards] = useState()
+  const [rows, setRows] = useState([createData('n/a', 0, 'n/a', 0, 'n/a', 0)])
 
   let cardId = ''
-  if(location.state && location.state.card) {
+  if (location.state && location.state.card) {
     const card = location.state.card;
     cardId = card.card_id
   }
 
-  const [rows, setRows] = useState([createData('n/a', 0, 'n/a', 0, 'n/a', 0)])
+  const [prices, setPrices] = useState([
+    ['Time', 'Value'], [
+      location.state.card.price_object.timestamp,
+      location.state.card.price_object.mean_value
+    ]])
 
   useEffect(() => {
     if (cardId) {
@@ -42,15 +47,17 @@ function Card(props){
         'x-api-key': 'VQi4PffXXeaUzTIaEBnzUaGdnP6sPy9EUWtZSdp8'
       }
       axios.get(url, {headers}).then((response) => {
-        if(response.status === 200) {
-          let card_name = response.data.path.substring(response.data.path.lastIndexOf('/')+1, response.data.path.length)
+        if (response.status === 200) {
+          let card_name = response.data.path.substring(response.data.path.lastIndexOf('/') + 1, response.data.path.length)
           setRows([createData(card_name, response.data.price_object.max_value,
-                                               response.data.condition_label,
-                                               response.data.price_object.mean_value,
-                                               response.data.condition_desc,
-                                               response.data.price_object.min_value,
-                                               response.data.label)])
+              response.data.condition_label,
+              response.data.price_object.mean_value,
+              response.data.condition_desc,
+              response.data.price_object.min_value,
+              response.data.label)])
         }
+      }).then(() => {
+        getPrices()
       })
     }
   }, [])
@@ -74,10 +81,33 @@ function Card(props){
       'x-api-key': 'VQi4PffXXeaUzTIaEBnzUaGdnP6sPy9EUWtZSdp8'
     }
     console.log('reanalyzing card: ' + cardId)
-    axios.post(url, {},{headers}).then((response) => {
-      if(response.status === 200)
-      {
+    axios.post(url, {}, {headers}).then((response) => {
+      if (response.status === 200) {
         window.location.reload()
+      } else {
+        console.log('error: ' + response.statusText)
+      }
+    })
+  }
+
+  const getPrices = (event) => {
+    const url = 'https://3zd6ttzexc.execute-api.us-east-1.amazonaws.com/prod/card/' + cardId + '/prices'
+    const headers = {
+      'Authorization': localStorage.getItem('id_token'),
+      'x-api-key': 'VQi4PffXXeaUzTIaEBnzUaGdnP6sPy9EUWtZSdp8'
+    }
+    axios.get(url, {headers}).then((response) => {
+      if (response.status === 200) {
+        let priceArray = [['Time', 'Value']]
+        response.data.prices.forEach((priceObject) => {
+          let price = [
+            priceObject.timestamp,
+            priceObject.mean_value
+          ]
+          priceArray.push(price)
+        })
+        setPrices(priceArray)
+        console.log(priceArray)
       } else {
         console.log('error: ' + response.statusText)
       }
@@ -108,9 +138,9 @@ function Card(props){
   return (
       <Container>
         <Header setCards={setCards}/>
-        <Grid container direction='row' style={{flex: 1, marginTop:10}}>
+        <Grid container direction='row' style={{flex: 1, marginTop: 15, marginBottom: 15}}>
           <TableContainer>
-            <Table style={{background:'#D4F1F4'}} sx={{marginTop: 16, minWidth: 600, minHeight:400 }} aria-label="simple table">
+            <Table style={{background: '#D4F1F4'}} sx={{minWidth: 600, minHeight: 400}} aria-label="simple table">
               <TableHead>
                 <TableRow>
                   <TableCell style={{fontSize: 30}}><strong>Card Features</strong></TableCell>
@@ -118,15 +148,15 @@ function Card(props){
               </TableHead>
               <TableBody>
                 <TableRow>
-                  <TableCell style={{ fontSize: 18}}><strong>Card Name</strong></TableCell>
-                  <TableCell style={{ fontSize: 18}}><strong>Maximum Value of Card</strong></TableCell>
+                  <TableCell style={{fontSize: 18}}><strong>Card Name</strong></TableCell>
+                  <TableCell style={{fontSize: 18}}><strong>Maximum Value of Card</strong></TableCell>
                 </TableRow>
                 <TableRow>
                   <TableCell style={{fontSize: 18}}>
                     <img key={location.state.card.path}
                          src={location.state.card.path}
                          alt={location.state.card.path}
-                         width={50} />
+                         width={50}/>
                     {rows[0].name}
                   </TableCell>
                   <TableCell style={{fontSize: 18}}>{rows[0].max_val}</TableCell>
@@ -155,13 +185,13 @@ function Card(props){
                 </TableRow>
                 <TableRow>
                   <TableCell style={{fontSize: 18}}>
-                    <Button variant="contained" style={{margin:5}} onClick={updateCard}>
+                    <Button variant="contained" style={{margin: 5}} onClick={updateCard}>
                       Update
                     </Button>
-                    <Button variant="contained" style={{margin:5}} onClick={reAnalyzeCard}>
+                    <Button variant="contained" style={{margin: 5}} onClick={reAnalyzeCard}>
                       Analyze
                     </Button>
-                    <Button variant="contained" style={{margin:5}} onClick={deleteCard}>
+                    <Button variant="contained" style={{margin: 5}} onClick={deleteCard}>
                       Delete
                     </Button>
                   </TableCell>
@@ -170,6 +200,21 @@ function Card(props){
             </Table>
           </TableContainer>
         </Grid>
+        <Paper>
+          <Chart
+              height={'600px'}
+              chartType="ScatterChart"
+              loader={<div>Loading Chart</div>}
+              data={prices}
+              options={{
+                title: 'Value of Card Over Time',
+                hAxis: {title: 'Time'},
+                vAxis: {title: 'Value'},
+                legend: 'none',
+              }}
+              rootProps={{'data-testid': '1'}}
+          />
+        </Paper>
         <CardList cards={cards}/>
       </Container>
   )
