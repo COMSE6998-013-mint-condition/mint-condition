a1243696c8f8c934672d653b8df747a5777a69da
Bharathi <bharathimurugan@gmail.com>
2021-12-20 09:36:15 -0500

    styling cleanup

--- Frontend/mint-condition-app/src/Components/ImageDialog.js
+++ Frontend/mint-condition-app/src/Components/ImageDialog.js
@@ -1,28 +1,15 @@
 import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
-import DialogContent from '@mui/material/DialogContent';
-import Grid from '@mui/material/Grid';
 
 export default function ImageDialog(props) {
     return (
-        <Dialog open={props.visible} onClose={props.handleClose} maxWidth={props.maxWidth}
-                maxHeight={false}>
-            <DialogContent style={{backgroundColor: "#575757"}}>
-                <Grid container
-                      justifyContent='space-evenly'
-                      alignItems='center'
-                      direction='column'
-                >
-                    <Grid item>
-                        <img
-                            src={props.item.src}
-                            srcSet={props.item.srcSet}
-                            alt={props.labels}
-                            loading="lazy"
-                            style={{"maxWidth": "100%", "objectFit": "cover"}}/>
-                    </Grid>
-                </Grid>
-            </DialogContent>
+        <Dialog open={props.visible} onClose={props.handleClose} maxWidth={props.maxWidth}>
+            <img
+                src={props.item.src}
+                srcSet={props.item.srcSet}
+                alt={props.labels}
+                loading="lazy"
+                style={{"width": 'auto', "maxHeight": "calc(100vh - 64px)", "objectFit": "contain"}}/>
         </Dialog>
     );
 }
